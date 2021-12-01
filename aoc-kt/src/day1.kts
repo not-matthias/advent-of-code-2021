690452319187e4c2077de71e2974d729c7204d90
not-matthias <26800596+not-matthias@users.noreply.github.com>
2021-12-01 14:51:01 +0100

    Simplified the lambdas

--- aoc-kt/src/day1.kts
+++ aoc-kt/src/day1.kts
@@ -3,11 +3,9 @@ import java.io.File
 fun main() {
     println("[Day 1]")
 
-    println(File("../../aoc-rs/input/2021/day1.txt").readLines().map { line -> line.toInt() }.windowed(2)
-        .count { window -> window[0] < window[1] })
-    println(File("../../aoc-rs/input/2021/day1.txt").readLines().map { line -> line.toInt() }.windowed(4)
-        .count { window -> window[0] + window[1] + window[2] < window[1] + window[2] + window[3] }
-    )
+    println(File("../../aoc-rs/input/2021/day1.txt").readLines().map { it.toInt() }.windowed(2).count { it[0] < it[1] })
+    println(File("../../aoc-rs/input/2021/day1.txt").readLines().map { it.toInt() }.windowed(4)
+        .count { it[0] + it[1] + it[2] < it[1] + it[2] + it[3] })
 }
 
 main()
