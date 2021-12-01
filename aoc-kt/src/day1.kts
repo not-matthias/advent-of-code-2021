bffca8f0c84fa8de316ce3d770d6babe884d650e
not-matthias <26800596+not-matthias@users.noreply.github.com>
2021-12-01 14:13:25 +0100

    Added solution for day1 in Kotlin

---
+++ aoc-kt/src/day1.kts
@@ -0,0 +1,14 @@
+import java.io.File
+
+fun main() {
+    println("[Day 1]");
+
+    println(File("../../aoc-rs/input/2021/day1.txt").readLines().map { line -> line.toInt() }.windowed(2)
+        .count { window -> window[0] < window[1] });
+
+    println(File("../../aoc-rs/input/2021/day1.txt").readLines().map { line -> line.toInt() }.windowed(4)
+        .count { window -> window[0] + window[1] + window[2] < window[1] + window[2] + window[3] }
+    );
+}
+
+main()
