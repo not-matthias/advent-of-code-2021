09512e59b660e78cd077fc863049f60aebb7189f
not-matthias <26800596+not-matthias@users.noreply.github.com>
2021-12-02 06:45:27 +0100

    Added second solution

--- aoc-rs/src/day2.rs
+++ aoc-rs/src/day2.rs
@@ -1,40 +1,34 @@
 use aoc_runner_derive::aoc;
 use aoc_runner_derive::aoc_generator;
+use std::ptr::null_mut;
 use std::str::FromStr;
 
+#[derive(Debug)]
 enum Command {
     Forward(u32),
     Down(u32),
     Up(u32),
 }
 
+#[aoc_generator(day2)]
 fn parse_input(input: &str) -> Vec<Command> {
     input
         .lines()
         .filter_map(|line| {
-            let mut parts = line.split(' ').collect::<Vec<_>>();
-
-            // let Some(command) = &parts[0] else { println!("command: {:?}", parts); return None; };
-            // let Some(num) = &parts[1] else { println!("num: {:?}", parts); return None; };
-            // let Ok(num) = u32::from_str(num.trim()) else { println!("num: {:?}", num); return None; };
+            // WARNING: When testing things, you must not call `nth` or `next` since it
+            //          consumes the iterator and will thus result in completely different
+            //          outputs. Either don't print anything, or just use `collect::<Vec<_>>()`.
             //
-            // Some(match command {
-            //     "forward" => Command::Forward(num),
-            //     "down" => Command::Forward(num),
-            //     "up" => Command::Forward(num),
-            //     _ => {
-            //         println!("Not handled: {:?}", line);
-            //         return None;
-            //     }
-            // })
-
-            let num = u32::from_str(parts[1].trim()).unwrap();
+            //          In this case, nth(1) and nth(0) also caused some problems for me, so
+            //          it's probably best to just use next() or next_back() instead.
+            //
+            let mut parts = line.split(' ').collect::<Vec<_>>();
+            let num = u32::from_str(parts[1]).unwrap();
             Some(match parts[0] {
-                "forward" => Command::Forward(num),
-                "down" => Command::Forward(num),
-                "up" => Command::Forward(num),
+                ("forward") => Command::Forward(num),
+                ("down") => Command::Forward(num),
+                ("up") => Command::Forward(num),
                 _ => {
-                    println!("Not handled: {:?}", line);
                     return None;
                 }
             })
@@ -42,90 +36,56 @@ fn parse_input(input: &str) -> Vec<Command> {
         .collect::<Vec<_>>()
 }
 
-// #[aoc_generator(day2)]
-// fn parse_input(input: &str) -> Vec<Command> {
-//     input
-//         .lines()
-//         .filter_map(|line| {
-//             let mut parts = line.split(' ');
-//             let num = u32::from_str(parts.nth(1).unwrap()).unwrap();
-//
-//             Some(match parts.as_str() {
-//                 "forward" => Command::Forward(num),
-//                 "down" => Command::Forward(num),
-//                 "up" => Command::Forward(num),
-//                 _ => {
-//                     println!("Not handled: {:?}", line);
-//                     return None;
-//                 }
-//             })
-//         })
-//         .collect::<Vec<_>>()
-// }
-
 #[aoc(day2, part1)]
-fn solve_part_1(input: &str) -> u32 {
+fn solve_part_1(input: &[Command]) -> u32 {
     let mut horizontal_position = 0;
     let mut depth = 0;
 
-    for line in input.lines() {
-        let mut parts = line.split(' ').collect::<Vec<_>>();
-
-        let num = u32::from_str(parts[1].trim()).unwrap();
-
-        match parts[0] {
-            "forward" => horizontal_position += num,
-            "down" => depth += num,
-            "up" => depth -= num,
-            _ => {
-                println!("Not handled: {:?}", line);
-                continue;
+    for command in input {
+        match command {
+            Command::Forward(a) => {
+                horizontal_position += a;
+            }
+            Command::Down(a) => {
+                depth += a;
+            }
+            Command::Up(a) => {
+                depth -= a;
             }
         }
     }
-
-    // for command in commands {
-    //     match command {
-    //         Command::Forward(a) => {
-    //             horizontal_position += a;
-    //         }
-    //         Command::Down(a) => {
-    //             depth += a;
-    //         }
-    //         Command::Up(a) => {
-    //             depth -= a;
-    //         }
-    //     }
-    // }
-
-    //
-    println!("horizontal_position: {:?}", horizontal_position);
-    println!("depth: {:?}", depth);
 
     depth * horizontal_position
 }
 
 #[aoc(day2, part2)]
-fn solve_part_2(input: &str) -> u32 {
+fn solve_part_2(input: &[Command]) -> u32 {
     let mut horizontal_position = 0;
     let mut depth = 0;
     let mut aim = 0;
 
-    for line in input.lines() {
-        let mut parts = line.split(' ').collect::<Vec<_>>();
-
-        let num = u32::from_str(parts[1].trim()).unwrap();
-
-        match parts[0] {
-            "forward" => {
-                horizontal_position += num;
-                depth = aim * num;
+    for command in input {
+        println!("{:?}", command);
+        match command {
+            // "forward" => {
+            //     horizontal_position += num;
+            //     depth = aim * num;
+            // }
+            // "down" => aim += num,
+            // "up" => aim -= num,
+            // _ => {
+            //     println!("Not handled: {:?}", line);
+            //     continue;
+            // }
+            Command::Forward(a) => {
+                horizontal_position += a;
+                depth += aim * a;
             }
-            "down" => aim += num,
-            "up" => aim -= num,
-            _ => {
-                println!("Not handled: {:?}", line);
-                continue;
+            Command::Down(a) => {
+                aim += a;
+            }
+            Command::Up(a) => {
+                aim -= a;
             }
         }
     }
