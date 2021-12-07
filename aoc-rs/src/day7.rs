7b27fc388dad6032e219e4a95e0fac5adec7a8f1
not-matthias <26800596+not-matthias@users.noreply.github.com>
2021-12-07 10:34:56 +0100

    Improved `parse_input`

--- aoc-rs/src/day7.rs
+++ aoc-rs/src/day7.rs
@@ -3,28 +3,22 @@ use rayon::prelude::*;
 use std::str::FromStr;
 
 #[aoc_generator(day7)]
-fn parse_input(input: &str) -> Vec<u16> {
-    input
-        .lines()
-        .map(|line| line.split(',').filter_map(|part| u16::from_str(part).ok()))
-        .flatten()
-        .collect::<Vec<_>>()
-}
+fn parse_input(input: &str) -> Vec<u32> { input.trim().split(',').flat_map(u32::from_str).collect::<Vec<_>>() }
 
 #[aoc(day7, part1)]
-fn solve_part_1(input: &[u16]) -> u16 {
+fn solve_part_1(input: &[u32]) -> u32 {
     let min = input.iter().min().unwrap();
     let max = input.iter().max().unwrap();
 
     (*min..*max)
         .into_par_iter()
-        .map(|i| input.iter().map(|input| i.abs_diff(*input)).sum::<u16>())
+        .map(|i| input.iter().map(|input| i.abs_diff(*input)).sum::<u32>())
         .min()
         .unwrap()
 }
 
 #[aoc(day7, part2)]
-fn solve_part_2(input: &[u16]) -> u16 {
+fn solve_part_2(input: &[u32]) -> u32 {
     let min = input.iter().min().unwrap();
     let max = input.iter().max().unwrap();
 
@@ -32,9 +26,13 @@ fn solve_part_2(input: &[u16]) -> u16 {
         .into_par_iter()
         .map(|i| {
             input
-                .into_par_iter()
-                .map(|input| (1..=input.abs_diff(i)).sum::<u16>())
-                .sum::<u16>()
+                .into_iter()
+                .map(|input| {
+                    let n = input.abs_diff(i);
+
+                    (n * (n + 1)) / 2
+                })
+                .sum::<u32>()
         })
         .min()
         .unwrap()
@@ -45,6 +43,12 @@ mod tests {
     use super::*;
 
     fn get_input() -> &'static str { "16,1,2,0,4,2,7,1,2,14" }
+
+    #[test]
+    fn test_parse_input() {
+        assert_eq!(parse_input(get_input()), vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14]);
+        assert_eq!(parse_input(include_str!("../input/2021/day7.txt")).len(), 1000);
+    }
 
     #[test]
     fn test_example() {
