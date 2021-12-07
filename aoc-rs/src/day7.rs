f9d34efe1cee9c31a09b2c0cd326567a7f8e64c8
not-matthias <26800596+not-matthias@users.noreply.github.com>
2021-12-07 09:12:54 +0100

    Added brute force solution for day7

--- aoc-rs/src/day7.rs
+++ aoc-rs/src/day7.rs
@@ -10,19 +10,8 @@ fn parse_input(input: &str) -> Vec<u32> {
         .collect::<Vec<_>>()
 }
 
-fn input_to_array(input: &[u32]) -> VecDeque<u64> {
-    let mut array = VecDeque::from([0u64; 9]);
-
-    for i in input {
-        array[(*i) as usize] += 1;
-    }
-
-    array
-}
-
-fn median(input: &[u32]) -> u32 { input.iter().sum::<u32>() / input.len() as u32 }
-
-fn bruteforce(input: &[u32]) -> u32 {
+#[aoc(day7, part1)]
+fn solve_part_1(input: &[u32]) -> u32 {
     let max = input.iter().max().unwrap();
 
     let mut min_costs = u32::MAX;
@@ -36,33 +25,22 @@ fn bruteforce(input: &[u32]) -> u32 {
     min_costs
 }
 
-#[aoc(day7, part1)]
-fn solve_part_1(input: &[u32]) -> u32 {
-    // let median = median(input);
-    // let costs = input.iter().map(|i| i.abs_diff(median)).sum::<u32>();
-    //
-    // // Check n+-5 elements around the median
-    // let mut min_median = median;
-    // let mut min_costs = costs;
-    // for i in -50..=50_i32 {
-    //     let costs = input.iter().map(|i| i.abs_diff(median + i)).sum::<u32>();
-    //     if costs < min_costs {
-    //         min_median = (median as i32 + i) as u32;
-    //         min_costs = costs;
-    //     }
-    // }
-    //
-    // // Option 1: Bruteforce
-    // // Option 2: Calculate median
-    //
-    // min_costs
-    bruteforce(input)
-}
-
 #[aoc(day7, part2)]
 fn solve_part_2(input: &[u32]) -> u32 {
-    //
-    todo!()
+    let max = input.iter().max().unwrap();
+
+    let mut min_costs = u32::MAX;
+    for i in 0..*max {
+        let costs = input
+            .iter()
+            .map(|input| (1..=input.abs_diff(i)).sum::<u32>())
+            .sum::<u32>();
+        if costs < min_costs {
+            min_costs = costs;
+        }
+    }
+
+    min_costs
 }
 
 #[cfg(test)]
@@ -77,7 +55,7 @@ mod tests {
         let input = parse_input(input);
 
         assert_eq!(solve_part_1(&input), 37);
-        // assert_eq!(solve_part_2(&input), 0);
+        assert_eq!(solve_part_2(&input), 168);
     }
 
     #[test]
@@ -85,5 +63,6 @@ mod tests {
         let input = parse_input(include_str!("../input/2021/day7.txt"));
 
         assert_eq!(solve_part_1(&input), 344138);
+        assert_eq!(solve_part_2(&input), 94862124);
     }
 }
